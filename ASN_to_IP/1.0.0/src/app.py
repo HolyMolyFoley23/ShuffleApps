8af33cbf202f55c7be582160a101342ec4102909
HolyMolyFoley23 <44344566+HolyMolyFoley23@users.noreply.github.com>
2020-07-03 22:08:14 -0500

    Update app.py

--- ASN_to_IP/1.0.0/src/app.py
+++ ASN_to_IP/1.0.0/src/app.py
@@ -39,11 +39,11 @@ class IP_to_ASN(AppBase):
         s.close()
         return ("".join(res))
 
-    async def IP_to_ASN(self,ips):
+    async def IP_to_ASN(self,ips,delimiter):
         # Team Cymru IP to ASN Mapping through 'netcat' (bulk use - few thousand per bulk use to minimize overall load as noted by the team cymru doc page).
         # Set output filename to include date+time
 
-
+        delim=delimiter.replace(r'\t','\t')
         valid_list=[]
         error_list=["Errors"]
         content="begin\nverbose\n"+ips+"\nend"
@@ -67,7 +67,7 @@ class IP_to_ASN(AppBase):
                 # Requested to convert CIDR into IP Range - from '68.22.187.0/24' to the following format: '68.22.187.0#~#24#~#68.22.187.255'
                 cidr_tail = cidr.split('/')[1]
                 cidr = ipaddress.ip_network(cidr)
-                cidr = '%s#~#%s#~#%s' % (cidr[0], cidr_tail, cidr[-1])
+                cidr = '%s%s%s%s%s' % (cidr[0], delim, cidr_tail, delim, cidr[-1])
 
                 # Test prints
                 '''
@@ -81,8 +81,9 @@ class IP_to_ASN(AppBase):
                 print ('%s#~#%s#~#%s#~#%s#~#%s#~#%s#~#%s' % (asn, ip, cidr, cc, registry, allocated, as_name) + '\n')
                 '''
                 #Append to Output
-
-                valid_list.append('%s#~#%s#~#%s#~#%s#~#%s#~#%s#~#%s' % (asn, ip, cidr, cc, registry, allocated, as_name) + '\n')
+                output=delim.join([asn, ip, cidr, cc, registry, allocated, as_name+'\n'])
+                valid_list.append(output)
+                #valid_list.append('%s#~#%s#~#%s#~#%s#~#%s#~#%s#~#%s' % (asn, ip, cidr, cc, registry, allocated, as_name) + '\n')
             else:
                 # If there's for some reason an unexpected number of columns (greater/lower than 6), the above parser would be inaccurate and need to analyze the given IP's output and update the script accordingly.
                 error_list.append(tc_record)
